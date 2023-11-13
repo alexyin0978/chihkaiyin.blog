b5f361cada3c560b4161d07bc49eab0ac094571b
Alex Yin <alex@indochat.co.id>
2023-11-13 15:22:47 +0800

    update: about content

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { Merriweather } from "next/font/google";
 
 import Nav from "@/components/Nav";
 import Avatar from "@/assets/avatar.jpeg";
+
+const merriweather = Merriweather({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export default function About() {
   return (
     <>
       <Nav />
       <div className="mt-10">
-        <div className="flex items-end">
+        <div className={`flex items-end ${merriweather.className}`}>
           <div className="w-16 h-16 rounded-full mr-4 border border-gray-400 overflow-hidden pb-5">
             <Image
               src={Avatar}
@@ -22,26 +28,29 @@ export default function About() {
           <h3 className="text-2xl">ChihKai Yin</h3>
         </div>
 
-        <div className="mt-5">
-          <p>
-            I graduated as an architecture student, currently a Web Frontend
-            Developer.
+        <div className="mt-8">
+          <p>Web Frontend Developer.</p>
+          <p className="mt-3">
+            Lover of architecture, movie, manga, coffee and aquarium.
           </p>
-          <p>Lover of architecture, movie, manga, coffee and aquarium.</p>
         </div>
-        <div className="mt-5 flex flex-col gap-2">
-          <p>Here is my social media:</p>
-          <ul className="dark:text-header_dark text-header">
-            <li>
-              <Link href="https://www.instagram.com/alexyin0978/">
-                Instagram
-              </Link>
-            </li>
-            <li>
-              <Link href="https://twitter.com/alexyin0978">Twitter</Link>
-            </li>
-          </ul>
-          <p>Feel free to chat with me!</p>
+
+        <div className="mt-3">
+          Chat with me via my{" "}
+          <span className=" text-header dark:text-header_dark">
+            <Link href="https://www.instagram.com/alexyin0978/">Instagram</Link>
+          </span>
+          ,
+          <span className="mx-1 text-header dark:text-header_dark">
+            <Link href="https://twitter.com/alexyin0978">Twitter</Link>
+          </span>
+          or
+          <span className="ml-1 text-header dark:text-header_dark">
+            <Link href="https://www.linkedin.com/in/alexyin0978/">
+              LinkedIn
+            </Link>
+          </span>
+          .
         </div>
       </div>
     </>
