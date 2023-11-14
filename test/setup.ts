83a90c3cc5df4867abf5f5fd86e824ee733799e3
Alex Yin <alex@indochat.co.id>
2023-11-14 12:34:00 +0800

    update: vitest basic setup

---
+++ test/setup.ts
@@ -0,0 +1,11 @@
+import { afterEach, expect } from "vitest";
+import { cleanup } from "@testing-library/react";
+import * as matchers from "@testing-library/jest-dom/matchers";
+
+// run the cleanup to clear jsdom after every test
+afterEach(() => {
+  cleanup();
+});
+
+// extend jest-dom matchers methods
+expect.extend(matchers);
