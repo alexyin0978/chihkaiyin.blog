994bd35a7ece8e0d702c0dc9a697e3fc7b3cf5a6
Alex Yin <alex@indochat.co.id>
2023-11-15 18:07:35 +0800

    update: move PostListItem out

--- app/page.test.tsx
+++ app/page.test.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 
-import { PostMetaData, getAllPostsMetaDatas } from "@/utils/readPost";
-import Home, { PostListItem } from "./page";
+import { getAllPostsMetaDatas } from "@/utils/readPost";
+import Home from "./page";
 
 describe("page Home: UI", () => {
   vi.mock("next/font/google", () => ({
@@ -18,31 +18,5 @@ describe("page Home: UI", () => {
     expect(main).toBeInTheDocument();
 
     expect(main.children.length).toBe(getAllPostsMetaDatas().length);
-  });
-
-  test("should render PostListItem properly", () => {
-    const mockPost: PostMetaData = {
-      title: "title",
-      date: "date",
-      subtitle: "subtitle",
-      fileName: "fileName",
-    };
-    render(<PostListItem {...mockPost} />);
-
-    const article = screen.getByTestId("home__main__article");
-    const link = screen.getByTestId("home__main__article__link");
-    const date = screen.getByTestId("home__main__article__date");
-    const subtitle = screen.getByTestId("home__main__article__subtitle");
-
-    expect(article).toBeInTheDocument();
-
-    expect(link).toBeInTheDocument();
-    expect(link).toHaveAttribute("href", `/${mockPost.fileName}`);
-
-    expect(date).toBeInTheDocument();
-    expect(date.innerHTML).toBe(mockPost.date);
-
-    expect(subtitle).toBeInTheDocument();
-    expect(subtitle.innerHTML).toBe(mockPost.subtitle);
   });
 });
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Link from "next/link";
-import { Merriweather } from "next/font/google";
-import { Montserrat } from "@next/font/google";
 
-import { getAllPostsMetaDatas, PostMetaData } from "@/utils/readPost";
+import { getAllPostsMetaDatas } from "@/utils/readPost";
 
 import { SelfIntro } from "@/components/SelfIntro";
+import { PostListItem } from "@/components/PostListItem";
 
 export const metadata = {
   title: {
@@ -45,50 +43,6 @@ export const metadata = {
     description: "I write things I have learned.",
   },
 };
-
-const merriweather = Merriweather({
-  weight: "400",
-  subsets: ["latin"],
-});
-
-const montserrat = Montserrat({ subsets: ["latin"] });
-
-export function PostListItem(props: PostMetaData) {
-  const { title, date, subtitle, fileName } = props;
-  return (
-    <article
-      data-testid="home__main__article"
-      className={`mt-14 text-black dark:text-white ${merriweather.className}`}
-    >
-      <header>
-        <h3
-          className={`text-3xl font-extrabold text-header dark:text-header_dark ${montserrat.className}`}
-        >
-          <Link
-            data-testid="home__main__article__link"
-            href={`/${fileName}`}
-          >
-            {title}
-          </Link>
-        </h3>
-        <div className="flex items-center gap-2 mt-2">
-          <small
-            className="text-xs"
-            data-testid="home__main__article__date"
-          >
-            {date}
-          </small>
-        </div>
-      </header>
-      <p
-        data-testid="home__main__article__subtitle"
-        className="mt-2"
-      >
-        {subtitle}
-      </p>
-    </article>
-  );
-}
 
 export default function Home() {
   const postMetaDatas = getAllPostsMetaDatas();
